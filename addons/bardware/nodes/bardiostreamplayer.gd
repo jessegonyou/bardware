e159c2eb16c61d17cea6106f867cb241b4475d04
Jesse Gonyou <jesse.gonyou@protonmail.com>
2026-05-16 15:02:34 -0400

    Add real-time patching

--- addons/bardware/nodes/bardiostreamplayer.gd
+++ addons/bardware/nodes/bardiostreamplayer.gd
@@ -8,6 +8,19 @@ extends AudioStreamPlayer
 ## This does not take effect at runtime
 @export var do_not_patch: bool = false
 
+## Load the patched version of some file into the
+## audio stream
+func bard_load(file: String):
+	var new_path = file.replacen("res://sound/", "user://patchbard/")
+	if file.ends_with(".wav"):
+		stream = AudioStreamWAV.load_from_file(new_path)
+	elif file.ends_with(".mp3"):
+		stream = AudioStreamMP3.load_from_file(new_path)
+	elif file.ends_with(".ogg"):
+		stream = AudioStreamOggVorbis.load_from_file(new_path)
+	else:
+		push_error("This stream type is not supported by Bardware")
+
 func _enter_tree() -> void:
 	if do_not_patch:
 		return
